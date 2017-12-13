f1cb05554a65800c1f15abd0fd55c11b132423b3
Marcelo Gutierrez <marcelo_gutierrez@villca.com>
2017-12-13 20:35:47 +0000

    changing test

--- test_flask.py
+++ test_flask.py
@@ -1,10 +1,20 @@
 
-from flask import Flask
-test_flask = Flask(__name__)
+# from flask import Flask
+# test_flask = Flask(__name__)
 
-@test_flask.route("/")
+# @test_flask.route("/")
+# def hello():
+#     return "<h1 style='color:blue'>Hello There!</h1>"
+# if __name__ == "__main__":
+#     #application.run(host='0.0.0.0', port='8080')
+#     test_flask.run(host='0.0.0.0', port='5000')
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
 if __name__ == "__main__":
-    #application.run(host='0.0.0.0', port='8080')
-    test_flask.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0')
